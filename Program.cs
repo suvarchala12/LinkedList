030cb2db45d456590d9e3bdd3b8cfebb3683442a
suvarchala12 <bhumasuvarchala55@gmail.com>
2021-12-28 12:04:59 +0530

    LinkedList

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace LinkedList
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LinkedList");
+
+        }
+    }
+}
+
