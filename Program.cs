eed92e7bd6bda2bd09987ac1b4b9577fe8faf623
suvarchala12 <bhumasuvarchala55@gmail.com>
2021-12-28 12:28:01 +0530

    uc5 deleting

--- Program.cs
+++ Program.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace DataStructureDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //LinkedList < int > list= new LinkedList<int>();
+            //list.AddLast(56);
+            //list.AddLast(30);
+            //list.AddLast(70);
+
             CustomLinkedList customLinkedList = new CustomLinkedList();
             customLinkedList.InsertLast(56);
+            customLinkedList.InsertLast(30);
             customLinkedList.InsertLast(70);
-            Console.WriteLine("Linked list before insertion:");
+            Console.WriteLine("Linked list before deletion:");
             customLinkedList.Display();
-            customLinkedList.InsertMid(30);
-            Console.WriteLine("Linked list after insertion:");
+            customLinkedList.DeleteFirstNode();
+            Console.WriteLine("Linked list after deletion:");
             customLinkedList.Display();
             Console.ReadLine();
         }
     }
-}
 }
